fbe23ebfd0dec1ce81e7cbeb7ef3b8c825d34c8f
ShifterBit <48891590+ShifterBit@users.noreply.github.com>
2021-06-24 17:44:51 -0400

    submission

--- script.js
+++ script.js
@@ -1,6 +1,24 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
+function compute() {
+  // Get Principal V
+  const principalValue = document.getElementById("principal").value;
+  // Make sure the principal value 0, a negative number or empty 
+  if (principalValue <= 0) {
+    alert("Enter a positive number.")
+    document.getElementById("principal").focus();
+  } else {
+    // Get the values from the users input and display them.
+    const numberOfYears = parseInt(document.getElementById("years").value);
+    const rate = document.getElementById("rate").value;
+    const interest = principalValue * numberOfYears * parseInt(rate) / 100;
+    const returnDate = parseInt(new Date().getFullYear() + years);
+    document.getElementById("result").innerHTML = `If you deposit ${principalValue} at an interest rate of ${rate}%.You will recieve an amount of ${interest}, in the year ${returnDate}`;
+  }
 }
-        
+
+
+
+function updateRate() {
+  var rateValue = document.getElementById("rate").value;
+  document.getElementById("rate_val").innerText = rateValue;
+}
+
